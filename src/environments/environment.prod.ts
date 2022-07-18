72fd546f2ff5f13e37c2d8f19d4b1369f9b59965
Jeremy21032 <98118966+Jeremy21032@users.noreply.github.com>
2022-07-17 19:49:57 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-  firebase: {
-    projectId: 'ionicimagen-358b2',
-    appId: '1:15695187033:web:16c4223e911597d9573548',
-    storageBucket: 'ionicimagen-358b2.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyCRwkLG7eBEkZJKa-1YnACLWKMb3frlu10',
-    authDomain: 'ionicimagen-358b2.firebaseapp.com',
-    messagingSenderId: '15695187033',
+ firebase: {
+    projectId: 'xxxxxxxxxxxxxxxxxxx',
+    appId: 'xxxxxxxxxxxxxxxxxxx',
+    storageBucket: 'xxxxxxxxxxxxxxxxxxx',
+    locationId: 'xxxxxxxxxxxxxxxxxxx',
+    apiKey: 'xxxxxxxxxxxxxxxxxxx',
+    authDomain: 'xxxxxxxxxxxxxxxxxxx',
+    messagingSenderId: 'xxxxxxxxxxxxxxxxxxx',
   },
   production: true
 };
